65bd6a9d65b7655affeca1ac5dab0b2e65c513b6
Juha S <juhseppa@gmail.com>
2024-05-01 13:52:29 +0300

    Valmis rajapintalaajennos, korjattu sqlite.js

--- database/sqlite.js
+++ database/sqlite.js
@@ -4,7 +4,7 @@ export const db = new sqlite3.Database('database/db.sqlite')
 
 db.exec("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL UNIQUE, password TEXT NOT NULL, age INTEGER, jti TEXT, role TEXT NOT NULL)STRICT")
 
-db.exec("CREATE TABLE IF NOT EXISTS events(id INTEGER PRIMARY KEY AUTOINCREMENT, event TEXT NOT NULL, location TEXT NOT NULL, event_date TEXT NOT NULL)")
+db.exec("CREATE TABLE IF NOT EXISTS events(id INTEGER PRIMARY KEY AUTOINCREMENT, event TEXT NOT NULL, location TEXT NOT NULL, event_date TEXT NOT NULL)STRICT")
 
 //db.serialize(()=>{
 
